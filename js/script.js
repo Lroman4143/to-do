e1c676fa133e2fe40e946245d3e345d5b15d1789
Lindsey Roman <lindsey.roman.geekwise@gmail.com>
2016-03-16 20:51:40 +0000

    Code Cleanup

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 document.addEventListener('DOMContentLoaded',function(event){
     var title = document.createElement('h1');
    var input = document.createElement('input');
-   var button = document.createElement('button')
+   var button = document.createElement('button');
    var h2 = document.createElement('h2');
    var hr = document.createElement('hr');
    
    title.textContent = 'My Personal To Do List';
-   
    button.textContent = 'submit';
    input.placeholder = 'Type List Item';
    h2.textContent = 'To Do:';
    
    var att = document.createAttribute("class");       
-    att.value = "z-depth-1";                           
-    button.setAttributeNode(att);
+   att.value = "z-depth-1";                           
+   button.setAttributeNode(att);
    
    input.style.width = '25rem';
-   
-  document.body.style.textAlign = 'center';
+   document.body.style.textAlign = 'center';
    button.style.display = 'block';
    button.style.marginLeft = 'auto';
    button.style.marginRight = 'auto';
@@ -32,14 +30,12 @@ document.addEventListener('DOMContentLoaded',function(event){
        var li = document.createElement('li');
        
        li.textContent = input.value;
-      li.style.textAlign = 'left';
-      li.style.marginLeft = '25rem';
-      li.style.marginRight = '25rem';
-      li.style.fontSize = '24px';
+       li.style.textAlign = 'left';
+       li.style.marginLeft = '25rem';
+       li.style.marginRight = '25rem';
+       li.style.fontSize = '24px';
     
        container.appendChild(li);
-       
-       
        container.insertBefore(li, container.firstChild);
    });
    
